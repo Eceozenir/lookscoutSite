ce6248de96dcaba168f9815740024110f8a445d2
Ece Özenir <ece@ozenir.org>
2024-05-01 14:03:37 +0300

    üzerinde çalışılıyor

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+function openModel() {
+    let modelContainer = document.querySelector("#modelContainer");
+    modelContainer.style.display = "block";
+}
+
+function closeModel() {
+    let modelContainer = document.querySelector("#modelContainer");
+    modelContainer.style.display = "none";
+}
+
